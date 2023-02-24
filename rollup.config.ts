b1b52da75a2b61a4995fe19c06643a70e88f0e09
youxia <hxg2021@gmail.com>
2023-02-24 18:00:06 +0800

    调整位置

--- rollup.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'rollup';
-export default defineConfig({
-    input: 'src/index.ts',
-    output: {
-        dir: 'dist',
-        file: 'index.js',
-        format: 'cjs'
-    }
-});
